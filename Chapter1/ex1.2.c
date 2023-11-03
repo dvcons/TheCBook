088c9dcb9d536b65cd70da8f9f07da8292392a73
dvcons <dvcon@icloud.com>
2023-11-03 15:20:54 +0100

    final changes

--- Chapter1/ex1.2.c
+++ Chapter1/ex1.2.c
@@ -1,2 +1,31 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
+/**
+ * This program prints prime pairs.
+*/
+
+bool isPrime(int n);
+
+int main() {
+    printf("(1, 3)\n");
+    for (int i = 1; i <= 200; i++) {
+        if (isPrime(i) && isPrime(i+2)) {
+            printf("(%d, %d)\n", i, i+2);
+        }
+    }
+    return(EXIT_SUCCESS);
+}
+
+bool isPrime(int n) {
+    if (n <=1) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
